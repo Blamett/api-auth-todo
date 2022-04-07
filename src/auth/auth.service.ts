a29ffcdd0a91cb4931702106557a9cdd051f47f0
blamet <arturbaldoramos@gmail.com>
2022-04-07 16:07:30 -0300

    implement login generate token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { throwError } from 'rxjs';
+import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
+import { UserPayload } from './models/UserPayload';
+import { UserToken } from './models/UserToke';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
+
+    login(user: User): UserToken {
+        
+        const payload: UserPayload = {
+            sub: user.id,
+            username: user.username
+        };
+
+        const jwtToken  = this.jwtService.sign(payload)
+
+        return{
+            access_token: jwtToken
+        }
+    }
 
     async validateUser(username: string, password: string) {
         const user = await this.userService.findByUsername(username);
